1236e8333ac1e0fd0afbf6248ceb8e0a38b812f5
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-08 18:15:21 +0000

    Created Navbar.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './styles/app.scss';
 import Sidebar from './components/Sidebar';
 import Hero from './components/Hero';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <section>
+        <Navbar />
         <Sidebar />
         <Hero />
         </section>
