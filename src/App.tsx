18f23ca47406a0cfabb0fb00c64a753dce111a5b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-08 18:12:33 +0000

    Created Hero.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './styles/app.scss';
 import Sidebar from './components/Sidebar';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
+      <section>
+        <Sidebar />
+        <Hero />
+        </section>
+      <section></section>
     </div>
   );
 }
