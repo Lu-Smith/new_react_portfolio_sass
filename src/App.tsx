e5405a3a4107b5380ade860d7e13b18da96109d2
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-08 17:59:16 +0000

    Added Sidebar to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './styles/app.scss';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hallo World</h1>
+      <Sidebar />
     </div>
   );
 }
