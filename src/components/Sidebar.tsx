12fc3301604392cc47eea752c2e6e068b5043dfa
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-08 20:31:49 +0000

    Updated Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className='Sidebar'>Sidebar</div>
+    <div className='Sidebar'>
+      <Link to='/'>Home</Link>
+      <Link to='/projects'>Projects</Link>
+      <Link to='/fineart'>Art</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/contact'>Contact</Link>
+    </div>
   )
 }
 
