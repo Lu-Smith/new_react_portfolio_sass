b604d208cc4682ba5d04c095f9b2068a0029a6af
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-08 21:26:40 +0000

    Updated Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,11 +4,19 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
     <div className='Sidebar'>
-      <Link to='/'>Home</Link>
-      <Link to='/projects'>Projects</Link>
-      <Link to='/fineart'>Art</Link>
-      <Link to='/about'>About</Link>
-      <Link to='/contact'>Contact</Link>
+      <div className="titleContainer">
+        <div className="imageContainer">
+          <img src="" alt="" />
+        </div>
+        <h2>Bridging<strong>Art</strong> and <strong>Web Development</strong></h2>
+      </div>
+      <div className="linkContainer">
+        <Link to='/'>Home</Link>
+        <Link to='/projects'>Projects</Link>
+        <Link to='/fineart'>Art</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+      </div>
     </div>
   )
 }
