f3d62b52738ca04a2ece25bc18dd5257be5ceee9
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-09 17:56:25 +0000

    Updated Projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,28 +1,38 @@
 import React from 'react';
 import projects from '../assets/content/projects';
+import Sidebar from './Sidebar';
 
-const Projects = () => {
+interface ModeProps {
+  mode: string,
+  handleMode: () => void,
+};
+
+const Projects = ({ mode, handleMode }: ModeProps) => {
   return (
     <div className='Projects'>
-      <h2>Projects</h2>
-      {projects.map((project, index) => {
-        return <div className={project.title} key={index}>
-          <h2>{project.title}</h2>
-          <div className="projectsContainer">
-            {project.project.map((item, index) => {
-              return <div key={index}>
-                <div className="project">
-                  <a href={item.href}>
-                    <h3>{item.title}</h3>
-                    <img src={item.src} alt={item.title} />
-                  </a>
+      <div className='menu'>
+        <Sidebar mode={mode} handleMode={handleMode} />
+      </div>
+      <div className="componentContainer">
+        <h2>Projects</h2>
+        {projects.map((project, index) => {
+          return <div className={project.title} key={index}>
+            <div className="projectsContainer">
+              <h3>{project.title}</h3>
+              {project.project.map((item, index) => {
+                return <div className="project" key={index}>
+                    {/* <a href={item.href}>
+                      <div className="imageContainer">
+                        <img src={item.src} alt={item.title} />
+                      </div>
+                      <h3>{item.title}</h3>
+                    </a> */}
                 </div>
-              </div>
-            })}
+              })}
+            </div>
           </div>
-        </div>
-      })}
-
+        })}
+      </div>
     </div>
   )
 }
