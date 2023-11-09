b14e12aa7960205ed80fd54529d556e20f667025
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-09 13:06:41 +0000

    Updated Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ProfileImage from '../assets/Luna_Smith.jpg';
+import { PiCoffee } from 'react-icons/pi';
+import { GrProjects } from 'react-icons/gr';
+import { TfiEmail, TfiInfoAlt } from 'react-icons/tfi';
+import { LuPalette } from 'react-icons/lu';
 
 const Sidebar = () => {
   return (
     <div className='Sidebar'>
       <div className="titleContainer">
-        <div className="imageContainer">
-          <img src={ProfileImage} alt="Luna Smith's profile" />
-        </div>
-        <h2>Luna Smith</h2>
+        <Link to="/">
+          <div className="imageContainer">
+            <img src={ProfileImage} alt="Luna Smith's profile" />
+          </div>
+          <h2><span>L</span>una <span>S</span>mith</h2>
+        </Link>
       </div>
       <div className="linkContainer">
-        <Link to='/'>Home</Link>
-        <Link to='/projects'>Projects</Link>
-        <Link to='/fineart'>Art</Link>
-        <Link to='/about'>About</Link>
-        <Link to='/contact'>Contact</Link>
+        <Link to='/'><PiCoffee />Home</Link>
+        <Link to='/projects'><GrProjects />Projects</Link>
+        <Link to='/fineart'><LuPalette />Art</Link>
+        <Link to='/about'><TfiInfoAlt />About</Link>
+        <Link to='/contact'><TfiEmail />Contact</Link>
       </div>
       <div className="box">
         <div className="container">
