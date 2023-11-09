5e1ebdab4d05118debeaef4c0bc85f49be351c77
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-09 20:01:42 +0000

    Updated Projects

--- src/assets/content/projects.js
+++ src/assets/content/projects.js
@@ -1,11 +1,16 @@
 //React
 import ImgR1 from '../media/projects/React/LuArtGalley_R.jpg';
 import ImgR2 from '../media/projects/React/WeatherApp_R.jpg';
-import ImgR3 from '../media/projects/React/TicTacToeR.jpg';
+import ImgR3 from '../media/projects/React/TicTacToe_R.jpg';
+import ImgR4 from '../media/projects/React/DicionaryApp_R.jpg';
+import ImgR5 from '../media/projects/React/RecipeApp_R.jpg';
 
 //JavaScript
 import ImgJ1 from '../media/projects/JavaScript/PixelRainJ.jpg';
 import ImgJ2 from '../media/projects/JavaScript/FroggerJ.jpg';
+import ImgJ3 from '../media/projects/JavaScript/RainbowSpikesJ.jpg';
+import ImgJ4 from '../media/projects/JavaScript/MemoryGameJ.jpg';
+import ImgJ5 from '../media/projects/JavaScript/MandalaShapesJ.jpg';
 
 
 const projects = [
@@ -51,6 +56,16 @@ const projects = [
                 title: 'TicTacToe',
                 href: 'https://tic-tac-toe-react-typescript-luna.netlify.app/',
                 src: ImgR3,
+            },
+            {
+                title: 'Dictionary App',
+                href: 'https://luminous-dictionary-app.netlify.app/',
+                src: ImgR4,
+            },
+            {
+                title: 'Recipe App',
+                href: 'https://recipe-app-react-luna-smith.netlify.app/',
+                src: ImgR5,
             }
         ]
     },
@@ -66,6 +81,21 @@ const projects = [
                 title: 'Frogger',
                 href: 'frogger-luna-smith.netlify.app',
                 src: ImgJ2,
+            },
+            {
+                title: 'RaibowSpikes',
+                href: 'https://rainbow-spikes-javascript-animation.netlify.app/',
+                src: ImgJ3,
+            },
+            {
+                title: 'Memory Game',
+                href: 'https://memory-game-crypto-coo.netlify.app/',
+                src: ImgJ4,
+            },
+            {
+                title: 'Mandala Shapes',
+                href: 'https://mandala-shapes.netlify.app/',
+                src: ImgJ5,
             }
         ]
     },
@@ -81,7 +111,12 @@ const projects = [
                 title: 'TicTacToe',
                 href: 'https://tic-tac-toe-react-typescript-luna.netlify.app/',
                 src: ImgR3,
-            }
+            },
+            {
+                title: 'Memory Game',
+                href: 'https://memory-game-crypto-coo.netlify.app/',
+                src: ImgJ4,
+            },
         ]
     },
 ]
