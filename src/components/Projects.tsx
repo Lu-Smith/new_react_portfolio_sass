f0190921372073dd36af736614d3fb6af4d7352a
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-09 19:11:53 +0000

    Updated Project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -21,16 +21,18 @@ const Projects = () => {
           return <div className={project.title} key={index}>
             <div className="projectsContainer">
               <h3>{project.title}</h3>
-              {project.project.map((item, index) => {
-                return <div className="project" key={index}>
-                    {/* <a href={item.href}>
-                      <div className="imageContainer">
-                        <img src={item.src} alt={item.title} />
-                      </div>
-                      <h3>{item.title}</h3>
-                    </a> */}
-                </div>
-              })}
+              <div>
+                {project.project.map((item, index) => {
+                  return <div className="project" key={index}>
+                      <a href={item.href}>
+                        <div className="imageContainer">
+                          {/* <img src={item.src} alt={item.title} /> */}
+                        </div>
+                        <h4>{item.title}</h4>
+                      </a>
+                  </div>
+                })}
+              </div>
             </div>
           </div>
         })}
