5bf2dd4d2b95448ed2e654251249d985b05ddf31
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-09 16:49:43 +0000

    Updated content of Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React from 'react';
 import projects from '../assets/content/projects';
-
 
 const Projects = () => {
   return (
@@ -8,9 +7,19 @@ const Projects = () => {
       <h2>Projects</h2>
       {projects.map((project, index) => {
         return <div className={project.title} key={index}>
-          {project.project.map((item, index) => {
-            return <div key={index}>{item.title}</div>
-          })}
+          <h2>{project.title}</h2>
+          <div className="projectsContainer">
+            {project.project.map((item, index) => {
+              return <div key={index}>
+                <div className="project">
+                  <a href={item.href}>
+                    <h3>{item.title}</h3>
+                    <img src={item.src} alt={item.title} />
+                  </a>
+                </div>
+              </div>
+            })}
+          </div>
         </div>
       })}
 
