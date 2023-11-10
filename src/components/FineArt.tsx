96c022dd8dca82778046af93ef948f9a7f7f1b54
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-10 11:56:38 +0000

    Updated FineArt.tsx

--- src/components/FineArt.tsx
+++ src/components/FineArt.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Sidebar from './Sidebar';
 
 const FineArt = () => {
+  const [mode, setMode] = useState('dark');
+
+  const handleMode = () => {
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
   return (
-    <div className='FineArt'>FineArt</div>
+    <div className={`FineArt ${mode === 'light' ? 'lightMode' : 'darkMode'}`}>
+      <div className='menu'>
+        <Sidebar mode={mode} handleMode={handleMode} />
+      </div>
+      <div className="componentContainer">
+        <h2>Fine Art</h2>
+      </div>
+    </div>
   )
 }
 
