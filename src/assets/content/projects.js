5f7c5d843858d94f31e22b906b4a156afbb259de
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-10 11:29:10 +0000

    Updated projects.js

--- src/assets/content/projects.js
+++ src/assets/content/projects.js
@@ -4,6 +4,10 @@ import ImgR2 from '../media/projects/React/WeatherApp_R.jpg';
 import ImgR3 from '../media/projects/React/TicTacToe_R.jpg';
 import ImgR4 from '../media/projects/React/DicionaryApp_R.jpg';
 import ImgR5 from '../media/projects/React/RecipeApp_R.jpg';
+import ImgR6 from '../media/projects/React/SnakeGame_R.jpg'
+import ImgR7 from '../media/projects/React/CandyHeavenShop_R.jpg';
+import ImgR8 from '../media/projects/React/BubbleCommunity_R.jpg';
+import ImgR9 from '../media/projects/React/Hangman_R.jpg';
 
 //JavaScript
 import ImgJ1 from '../media/projects/JavaScript/PixelRainJ.jpg';
@@ -11,6 +15,10 @@ import ImgJ2 from '../media/projects/JavaScript/FroggerJ.jpg';
 import ImgJ3 from '../media/projects/JavaScript/RainbowSpikesJ.jpg';
 import ImgJ4 from '../media/projects/JavaScript/MemoryGameJ.jpg';
 import ImgJ5 from '../media/projects/JavaScript/MandalaShapesJ.jpg';
+import ImgJ6 from '../media/projects/JavaScript/ExampleShop_J.png';
+import ImgJ7 from '../media/projects/JavaScript/JSTextAnimation_J.jpg';
+import ImgJ8 from '../media/projects/JavaScript/Matrix_J.jpg';
+import ImgJ9 from '../media/projects/JavaScript/RockPaperScissors_J.jpg';
 
 
 const projects = [
@@ -66,7 +74,27 @@ const projects = [
                 title: 'Recipe App',
                 href: 'https://recipe-app-react-luna-smith.netlify.app/',
                 src: ImgR5,
-            }
+            },
+            {
+                title: 'Snake Game',
+                href: 'https://snake-game-react-typescript.netlify.app/',
+                src: ImgR6,
+            },
+            {
+                title: 'Candy Heaven',
+                href: 'https://candyheavenshop.netlify.app/',
+                src: ImgR7,
+            },
+            {
+                title: 'Bubble - community space',
+                href: 'https://community-fans.netlify.app/',
+                src: ImgR8,
+            },
+            {
+                title: 'Hangman',
+                href: 'https://hangman-react-typescript.netlify.app/',
+                src: ImgR9,
+            } 
         ]
     },
     {
@@ -96,6 +124,26 @@ const projects = [
                 title: 'Mandala Shapes',
                 href: 'https://mandala-shapes.netlify.app/',
                 src: ImgJ5,
+            },
+            {
+                title: 'Example Shop',
+                href: 'https://resonant-mermaid-b21b5e.netlify.app/',
+                src: ImgJ6,
+            },
+            {
+                title: 'JS Text Animations',
+                href: 'https://ephemeral-bienenstitch-24f392.netlify.app/',
+                src: ImgJ7,
+            },
+            {
+                title: 'Matrix',
+                href: 'https://matrix-js.netlify.app/',
+                src: ImgJ8,
+            },
+            {
+                title: 'Rock Paper Scissors',
+                href: 'https://rockpaperscissors-fun-game.netlify.app/',
+                src: ImgJ9,
             }
         ]
     },
