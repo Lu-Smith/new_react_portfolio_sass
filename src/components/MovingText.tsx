618305bccbc2716469c98ecf9862a9f140562609
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-10 19:10:25 +0000

    Updated MovinText

--- src/components/MovingText.tsx
+++ src/components/MovingText.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const sliderVariants = {
+    initial: {
+        x: 0,
+    },
+    animate: {
+        x: "-220%",
+        transition: {
+            duration: 30,
+            repeat: Infinity,                
+        }
+    },
+}
 
 const MovingText = () => {
   return (
-    <div>MovingText</div>
+    <motion.div 
+    className="slidingTextContainer" 
+    variants={sliderVariants}
+    initial="initial"
+    animate="animate">
+        Creator of code-based masterpieces and fine art.
+    </motion.div>
   )
 }
 
