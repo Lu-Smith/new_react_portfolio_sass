8cc0acd4a2d71a4a2f0e84c9c9bf0cd5560cf69d
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-10 15:55:17 +0000

    Updated About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -22,11 +22,19 @@ const About = () => {
       </div>
       <div className="componentContainer">
         <h2>About</h2>
-        <div className="imageContainer">
-          <img src={BioImage} alt="Luna Smith" />
+        <div className="container">
+          <div className="imageContainer">
+            <img src={BioImage} alt="Luna Smith" />
+          </div>
+          <div className="bio">
+            <p>I am a professional artist and web developer from Edinburgh, Scotland, deeply rooted in the rich culture and landscapes of my homeland. Since my early years, a profound love for nature and art has guided my creative journey. Winning my first art competition in primary school ignited a passion that has withstood challenges and obstacles, shaping my distinctive artistic style.</p>
+            <p>My artistic evolution has been influenced by diverse experiences living in various countries, exposing me to different languages, cultures, customs, architecture, and natural environments. Specializing in oil painting, I am captivated by the medium's thick texture, using vigorous brushstrokes and expressive finger-work to not only capture the surface but also the soul of my subjects</p>
+            <p>With two permanent exhibitions in Italy and China, and a solo exhibition in Edinburgh, my vibrant and energy-infused paintings are sought after by collectors worldwide. Beyond art, I have a profound interest in coding, Norse Mythology, and Celtic culture, finding inspiration in their stories and symbols, which often manifest in the themes and motifs of my work.</p>
+            <p>Through my art, I express a deep connection to life, nature, and cultural traditions. My legacy as a talented Scottish painter with a unique perspective is a testament to my enduring passion for capturing the essence of the world around me.</p>
+          </div>
+          <button onClick={handleEvents}>Events</button>
+          {displayEvents && <Events />}
         </div>
-        <button onClick={handleEvents}>Events</button>
-        {displayEvents && <Events />}
       </div>
     </div>
   )
