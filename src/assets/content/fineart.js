26c2d90ae7bd579891e74faed5649be39de90ec3
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-10 12:36:29 +0000

    Updated fineart.js

---
+++ src/assets/content/fineart.js
@@ -0,0 +1,231 @@
+//Nott
+import ImgN1 from '../media/fineart/Nott/Awakening-from-the-Sleep-of-Fear-by-Luna-Smith.jpg';
+import ImgN2 from '../media/fineart/Nott/The-Warrior-of-Shattered-Tears-by-Luna-Smith.jpg';
+import ImgN3 from '../media/fineart/Nott/Dreams-of-a-Future-Self-by-Luna-Smith.jpg';
+import ImgN4 from '../media/fineart/Nott/The-Soothing-Wind-of-Longing-by-Luna-Smith.jpg';
+import ImgN5 from '../media/fineart/Nott/The-Battle-of-Endless-Seas-by-Luna-Smith.jpg';
+import ImgN6 from '../media/fineart/Nott/The-Flame-of-the-Eternal-Triangle-by-Luna-Smith.jpg';
+import ImgN7 from '../media/fineart/Nott/The-Blood-of-the-Silent-Unicorn-by-Luna-Smith.jpg';
+import ImgN8 from '../media/fineart/Nott/Breathe-of-the-Invisible-Beast-by-Luna-Smith.jpg';
+import ImgN9 from '../media/fineart/Nott/The-Heartbeat-of-the-Raging-Cyclone-by-Luna-Smith.jpg';
+import ImgN10 from '../media/fineart/Nott/The-Scale-of-the-Blue-Dragon-by-Luna-Smith.jpg';
+import ImgN11 from '../media/fineart/Nott/The-Temple-of-Life-by-Luna-Smith.jpg';
+import ImgN12 from '../media/fineart/Nott/Kiss-of-Mothers-Love-by-Luna-Smith.jpg';
+import ImgN13 from '../media/fineart/Nott/Songs-of-the-Burning-Star-by-Luna-Smith.jpg';
+import ImgN14 from '../media/fineart/Nott/The-Fading-Reality-by-Luna-Smith.jpg';
+import ImgN15 from '../media/fineart/Nott/The-Reflection-of-my-Universe-by-Luna-Smith.jpg';
+
+//Landscapes
+import ImgL1 from '../media/projects/JavaScript/PixelRainJ.jpg';
+
+//Animals
+import ImgA1 from '../media/projects/JavaScript/PixelRainJ.jpg';
+
+//SoundofLife
+import ImgS1 from '../media/projects/JavaScript/PixelRainJ.jpg';
+
+const paintings = [
+    {
+        title: 'Nott',
+        project: [
+            {
+                title: 'Awakening from the Sleep of Fear',
+                src: ImgN1,
+            },
+            {
+                title: 'The Warrior of Shattered Tears',
+                src: ImgN2,
+            },
+            {
+                title: 'Dreams of a Future Self',
+                src: ImgN3,
+            },
+            {
+                title: 'The Soothing Wind of Longing',
+                src: ImgN4,
+            },
+            {
+                title: 'The Battle of Endless Seas',
+                src: ImgN5,
+            },
+            {
+                title: 'The Flame of the Eternal Triangle',
+                src: ImgN6,
+            },
+            {
+                title: 'The Blood of the Silent Unicorn',
+                src: ImgN7,
+            },
+            {
+                title: 'Breathe of the Invisible Beast',
+                src: ImgN8,
+            },
+            {
+                title: 'The Heartbeat of the Raging Cyclone',
+                src: ImgN9,
+            },
+            {
+                title: 'A Scale of the Blue Dragon',
+                src: ImgN10,
+            },
+            {
+                title: 'The Temple of Life',
+                src: ImgN11,
+            },
+            {
+                title: "Kiss of Mother's Love",
+                src: ImgN12,
+            },
+            {
+                title: 'Songs of the Burning Star',
+                src: ImgN13,
+            },
+            {
+                title: "The Fading Reality",
+                src: ImgN14,
+            },
+            {
+                title: "The Reflection of My Universe",
+                src: ImgN15,
+            },
+            
+        ]
+    },
+    {
+        title: 'React paintings',
+        project: [
+            {
+                title: 'LuArt Gallery',
+                href: 'https://luartgallery.com/',
+                src: ImgR1,
+            },
+            {
+                title: 'Weather App',
+                href: 'https://weather-app-react-luna-smith.netlify.app/',
+                src: ImgR2,
+            },
+            {
+                title: 'TicTacToe',
+                href: 'https://tic-tac-toe-react-typescript-luna.netlify.app/',
+                src: ImgR3,
+            },
+            {
+                title: 'Dictionary App',
+                href: 'https://luminous-dictionary-app.netlify.app/',
+                src: ImgR4,
+            },
+            {
+                title: 'Recipe App',
+                href: 'https://recipe-app-react-luna-smith.netlify.app/',
+                src: ImgR5,
+            },
+            {
+                title: 'Snake Game',
+                href: 'https://snake-game-react-typescript.netlify.app/',
+                src: ImgR6,
+            },
+            {
+                title: 'Candy Heaven',
+                href: 'https://candyheavenshop.netlify.app/',
+                src: ImgR7,
+            },
+            {
+                title: 'Bubble - community space',
+                href: 'https://community-fans.netlify.app/',
+                src: ImgR8,
+            },
+            {
+                title: 'Hangman',
+                href: 'https://hangman-react-typescript.netlify.app/',
+                src: ImgR9,
+            } 
+        ]
+    },
+    {
+        title: 'JavaScript paintings',
+        project: [
+            {
+                title: 'Pixel Rain ',
+                href: 'https://pixel-rain.netlify.app/',
+                src: ImgJ1,
+            },
+            {
+                title: 'Frogger',
+                href: 'frogger-luna-smith.netlify.app',
+                src: ImgJ2,
+            },
+            {
+                title: 'RaibowSpikes',
+                href: 'https://rainbow-spikes-javascript-animation.netlify.app/',
+                src: ImgJ3,
+            },
+            {
+                title: 'Memory Game',
+                href: 'https://memory-game-crypto-coo.netlify.app/',
+                src: ImgJ4,
+            },
+            {
+                title: 'Mandala Shapes',
+                href: 'https://mandala-shapes.netlify.app/',
+                src: ImgJ5,
+            },
+            {
+                title: 'Example Shop',
+                href: 'https://resonant-mermaid-b21b5e.netlify.app/',
+                src: ImgJ6,
+            },
+            {
+                title: 'JS Text Animations',
+                href: 'https://ephemeral-bienenstitch-24f392.netlify.app/',
+                src: ImgJ7,
+            },
+            {
+                title: 'Matrix',
+                href: 'https://matrix-js.netlify.app/',
+                src: ImgJ8,
+            },
+            {
+                title: 'Rock Paper Scissors',
+                href: 'https://rockpaperscissors-fun-game.netlify.app/',
+                src: ImgJ9,
+            }
+        ]
+    },
+    {
+        title: 'Games',
+        project: [
+            {
+                title: 'Frogger',
+                href: 'frogger-luna-smith.netlify.app',
+                src: ImgJ2,
+            },
+            {
+                title: 'TicTacToe',
+                href: 'https://tic-tac-toe-react-typescript-luna.netlify.app/',
+                src: ImgR3,
+            },
+            {
+                title: 'Memory Game',
+                href: 'https://memory-game-crypto-coo.netlify.app/',
+                src: ImgJ4,
+            },
+            {
+                title: 'Snake Game',
+                href: 'https://snake-game-react-typescript.netlify.app/',
+                src: ImgR6,
+            },
+            {
+                title: 'Hangman',
+                href: 'https://hangman-react-typescript.netlify.app/',
+                src: ImgR9,
+            },
+            {
+                title: 'Rock Paper Scissors',
+                href: 'https://rockpaperscissors-fun-game.netlify.app/',
+                src: ImgJ9,
+            } 
+        ]
+    },
+]
+
+export default paintings;
