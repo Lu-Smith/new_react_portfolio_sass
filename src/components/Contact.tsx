7985b3ae0dc326a3603a858f9fce9e84226eb20b
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-10 14:20:46 +0000

    Updated Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Sidebar from './Sidebar';
 
 const Contact = () => {
+
+  const [mode, setMode] = useState('dark');
+
+  const handleMode = () => {
+    setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
   return (
-    <div className='Contact'>Contact</div>
+    <div className={`Contact ${mode === 'light' ? 'lightMode' : 'darkMode'}`}>
+      <div className='menu'>
+        <Sidebar mode={mode} handleMode={handleMode} />
+      </div>
+    </div>
   )
 }
 
