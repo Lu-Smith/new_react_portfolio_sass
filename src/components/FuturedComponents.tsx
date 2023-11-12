ed128ac6b99ce63a8b9c86ffacebb943cd574fec
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-12 19:36:01 +0000

    Updated FuturedComponents

--- src/components/FuturedComponents.tsx
+++ src/components/FuturedComponents.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 interface ModeProps {
   mode: string,
 };
 
+const futuredVariants = {
+  initial: {
+      y: -500,
+      opacity: 0,
+  },
+  animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+          duration: 1,
+          staggerChildren: 0.1,
+      }
+  },
+}
+
 const FuturedComponents = ({ mode }: ModeProps) => {
   return (
-    <div className={`FuturedComponents ${mode === 'light' ? 'lightMode' : 'darkMode'}`}>
+    <motion.div 
+    className={`FuturedComponents 
+    ${mode === 'light' ? 'lightMode' : 'darkMode'}`}
+    variants={futuredVariants}
+    initial="initial"
+    animate="animate">
          <Link to='/projects' className="boxContainer">
           <h2>100+</h2>
           <span>Completed Projects</span>
@@ -24,7 +45,7 @@ const FuturedComponents = ({ mode }: ModeProps) => {
             <h2>Years</h2>
           <span>of Experience</span>
         </Link>
-    </div>
+    </motion.div>
   )
 }
 
