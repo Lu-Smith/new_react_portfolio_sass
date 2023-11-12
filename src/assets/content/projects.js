299a643b058f2f5208c080f2cb2f511089b62525
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-12 13:00:05 +0000

    Updated projects.js

--- src/assets/content/projects.js
+++ src/assets/content/projects.js
@@ -10,15 +10,21 @@ import ImgR8 from '../media/projects/React/BubbleCommunity_R.jpg';
 import ImgR9 from '../media/projects/React/Hangman_R.jpg';
 
 //JavaScript
-import ImgJ1 from '../media/projects/JavaScript/PixelRainJ.jpg';
-import ImgJ2 from '../media/projects/JavaScript/FroggerJ.jpg';
-import ImgJ3 from '../media/projects/JavaScript/RainbowSpikesJ.jpg';
-import ImgJ4 from '../media/projects/JavaScript/MemoryGameJ.jpg';
-import ImgJ5 from '../media/projects/JavaScript/MandalaShapesJ.jpg';
+import ImgJ1 from '../media/projects/JavaScript/JustForFun_J.jpg';
+import ImgJ2 from '../media/projects/JavaScript/Frogger_J.jpg';
+import ImgJ3 from '../media/projects/JavaScript/RainbowSpikes_J.jpg';
+import ImgJ4 from '../media/projects/JavaScript/MemoryGame_J.jpg';
+import ImgJ5 from '../media/projects/JavaScript/MandalaShapes_J.jpg';
 import ImgJ6 from '../media/projects/JavaScript/ExampleShop_J.png';
 import ImgJ7 from '../media/projects/JavaScript/JSTextAnimation_J.jpg';
 import ImgJ8 from '../media/projects/JavaScript/Matrix_J.jpg';
 import ImgJ9 from '../media/projects/JavaScript/RockPaperScissors_J.jpg';
+import ImgJ10 from '../media/projects/JavaScript/SASS_J.jpg';
+import ImgJ11 from '../media/projects/JavaScript/FollowMe_J.jpg';
+import ImgJ12 from '../media/projects/JavaScript/TeamGenerator_J.png';
+import ImgJ13 from '../media/projects/JavaScript/PlayWithCanvas_J.jpg';
+import ImgJ14 from '../media/projects/JavaScript/JS Animations_J.jpg';
+import ImgJ15 from '../media/projects/JavaScript/PixelRain_J.jpg';
 
 
 const projects = [
@@ -36,8 +42,8 @@ const projects = [
                 src: ImgR2,
             },
             {
-                title: 'Pixel Rain ',
-                href: 'https://pixel-rain.netlify.app/',
+                title: 'Just for FUN',
+                href: 'https://just-for-fun-js.netlify.app/',
                 src: ImgJ1,
             },
             {
@@ -111,8 +117,8 @@ const projects = [
         title: 'JavaScript Projects',
         project: [
             {
-                title: 'Pixel Rain ',
-                href: 'https://pixel-rain.netlify.app/',
+                title: 'Just for FUN',
+                href: 'https://just-for-fun-js.netlify.app/',
                 src: ImgJ1,
             },
             {
@@ -154,7 +160,38 @@ const projects = [
                 title: 'Rock Paper Scissors',
                 href: 'https://rockpaperscissors-fun-game.netlify.app/',
                 src: ImgJ9,
-            }
+            },
+            {
+                title: 'SASS',
+                href: 'https://sass-my-first-website.netlify.app/',
+                src: ImgJ10,
+            },
+            {
+                title: 'Follow me',
+                href: 'https://canvas-project-js.netlify.app/',
+                src: ImgJ11,
+            },
+            {
+                title: 'Team Generator',
+                href: 'https://elaborate-caramel-f27ed1.netlify.app/',
+                src: ImgJ12,
+            },
+            {
+                title: 'Play with CANVAS',
+                href: 'https://multiple-canvases-js.netlify.app/',
+                src: ImgJ13,
+            },
+            {
+                title: 'JS Animations',
+                href: 'https://js-animations-luna-smith.netlify.app/',
+                src: ImgJ14,
+            },
+            {
+                title: 'Pixel Rain ',
+                href: 'https://pixel-rain.netlify.app/',
+                src: ImgJ15,
+            },
+            
         ]
     },
     {
@@ -189,7 +226,17 @@ const projects = [
                 title: 'Rock Paper Scissors',
                 href: 'https://rockpaperscissors-fun-game.netlify.app/',
                 src: ImgJ9,
-            } 
+            },
+            {
+                title: 'Play with CANVAS',
+                href: 'https://multiple-canvases-js.netlify.app/',
+                src: ImgJ13,
+            },
+            {
+                title: 'Follow me',
+                href: 'https://canvas-project-js.netlify.app/',
+                src: ImgJ11,
+            },
         ]
     },
 ]
