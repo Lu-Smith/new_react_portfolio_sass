47bcf4303cee8e79e0b8a6f24b5a962f92888364
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-14 11:56:27 +0000

    Updated webpack

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,12 @@
 module.exports = {
-    webpack: (config) => {
-      config.resolve.fallback = {
-        "path": require.resolve("path-browserify"),
-        "os": require.resolve("os-browserify/browser"),
-        "crypto": require.resolve("crypto-browserify")
-      };
-      return config;
-    }
-  };
+  webpack: (config) => {
+    config.resolve.fallback = {
+      "path": require.resolve("path-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "buffer": require.resolve("buffer")
+    };
+    return config;
+  }
+};
