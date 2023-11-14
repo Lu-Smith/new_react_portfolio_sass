c05712f41fb6e59700a82fc47b402bff4aebc454
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2023-11-14 11:53:53 +0000

    Updated webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
+import 'dotenv/config';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'dotenv/config';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
