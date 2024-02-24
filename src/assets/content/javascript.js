fdd95a40c39c37d63c9045a1d5d514e503f2ead9
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-24 17:10:04 +0000

    Created javascript.js

---
+++ src/assets/content/javascript.js
@@ -0,0 +1,120 @@
+//JavaScript
+import ImgJ1 from '../media/projects/JavaScript/JustForFun_J.jpg';
+import ImgJ2 from '../media/projects/JavaScript/Frogger_J.jpg';
+import ImgJ3 from '../media/projects/JavaScript/RainbowSpikes_J.jpg';
+import ImgJ4 from '../media/projects/JavaScript/MemoryGame_J.jpg';
+import ImgJ5 from '../media/projects/JavaScript/MandalaShapes_J.jpg';
+import ImgJ6 from '../media/projects/JavaScript/ExampleShop_J.png';
+import ImgJ7 from '../media/projects/JavaScript/JSTextAnimation_J.jpg';
+import ImgJ8 from '../media/projects/JavaScript/Matrix_J.jpg';
+import ImgJ9 from '../media/projects/JavaScript/RockPaperScissors_J.jpg';
+import ImgJ10 from '../media/projects/JavaScript/SASS_J.jpg';
+import ImgJ11 from '../media/projects/JavaScript/FollowMe_J.jpg';
+import ImgJ12 from '../media/projects/JavaScript/TeamGenerator_J.png';
+import ImgJ13 from '../media/projects/JavaScript/PlayWithCanvas_J.jpg';
+import ImgJ14 from '../media/projects/JavaScript/JS Animations_J.jpg';
+import ImgJ15 from '../media/projects/JavaScript/PixelRain_J.jpg';
+import ImgJ16 from '../media/projects/JavaScript/ConnectFour_J.jpg';
+import ImgJ17 from '../media/projects/JavaScript/SpaceInvaders_J.jpg';
+import ImgJ18 from '../media/projects/JavaScript/FlowFields_J.jpg';
+
+const projects = [
+    {
+        title: 'JavaScript Projects',
+        project: [
+            {
+                title: 'Frogger',
+                href: 'frogger-luna-smith.netlify.app',
+                src: ImgJ2,
+            },
+            {
+                title: 'Raibow Spikes',
+                href: 'https://rainbow-spikes-javascript-animation.netlify.app/',
+                src: ImgJ3,
+            },
+            {
+                title: 'Memory Game',
+                href: 'https://memory-game-crypto-coo.netlify.app/',
+                src: ImgJ4,
+            },
+            {
+                title: 'Mandala Shapes',
+                href: 'https://mandala-shapes.netlify.app/',
+                src: ImgJ5,
+            },
+            {
+                title: 'Example Shop',
+                href: 'https://resonant-mermaid-b21b5e.netlify.app/',
+                src: ImgJ6,
+            },
+            {
+                title: 'Text Animations',
+                href: 'https://ephemeral-bienenstitch-24f392.netlify.app/',
+                src: ImgJ7,
+            },
+            {
+                title: 'Team Generator',
+                href: 'https://elaborate-caramel-f27ed1.netlify.app/',
+                src: ImgJ12,
+            },
+            {
+                title: 'Matrix',
+                href: 'https://matrix-js.netlify.app/',
+                src: ImgJ8,
+            },
+            {
+                title: 'Rock Paper Scissors',
+                href: 'https://rockpaperscissors-fun-game.netlify.app/',
+                src: ImgJ9,
+            },
+            {
+                title: 'SASS',
+                href: 'https://sass-my-first-website.netlify.app/',
+                src: ImgJ10,
+            },
+            {
+                title: 'Flow Fields',
+                href: 'https://flow-fields-javascripst.netlify.app/',
+                src: ImgJ18,
+            },
+            {
+                title: 'Just for FUN',
+                href: 'https://just-for-fun-js.netlify.app/',
+                src: ImgJ1,
+            },
+            {
+                title: 'Play with CANVAS',
+                href: 'https://canvas-project-js.netlify.app/',
+                src: ImgJ13,
+            },
+            {
+                title: 'JS Animations',
+                href: 'https://js-animations-luna-smith.netlify.app/',
+                src: ImgJ14,
+            },
+            {
+                title: 'Pixel Rain ',
+                href: 'https://pixel-rain.netlify.app/',
+                src: ImgJ15,
+            },
+            {
+                title: 'Connect Four',
+                href: 'connect-four-luna-smith.netlify.app',
+                src: ImgJ16,
+            },
+            {
+                title: 'Follow me',
+                href: 'https://multiple-canvases-js.netlify.app/',
+                src: ImgJ11,
+            },
+            {
+                title: 'Space Invaders',
+                href: 'https://space-invaders-luna-smith.netlify.app/',
+                src: ImgJ17,
+            },
+            
+        ]
+    },
+]
+
+export default projects;
