cfefa07ae283564d71c170a407e54be5d740eaa6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-24 17:09:38 +0000

    Created ReactProjects.tsx

---
+++ src/components/Projects/ReactProjects.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import projects from '../../assets/content/react';
+import { LiaExternalLinkAltSolid } from 'react-icons/lia';
+import Sidebar from '../Sidebar/Sidebar';
+import MobileSidebar from '../Sidebar/MobileSidebar';
+
+interface ModeProps {
+  mode: string,
+  handleMode: () => void,
+};
+
+const ReactProjects = ( {mode, handleMode}: ModeProps ) => {
+
+  return (
+    <div className={`Games ${mode === 'light' ? 'lightMode' : 'darkMode'}`}>
+      <div className='menu'>
+        <Sidebar mode={mode} handleMode={handleMode} />
+      </div>
+      <div className='menuMobile'>
+        <MobileSidebar mode={mode} handleMode={handleMode} />
+      </div>
+        <div className="componentContainer">
+          <h2>React Projects</h2>
+          {projects.map((project, index) => {
+          return <div key={index}>
+            {project.title === 'React Projects' && 
+            <div className="projectsContainer">
+              <div 
+              className='game'>
+                {project.project.map((item, index) => {
+                  return <a href={item.href} key={index} target='_blank' rel="noreferrer">
+                        <div className="imageContainer">
+                          <img src={item.src} alt={item.title} />
+                        </div>
+                        <h4>{item.title}<LiaExternalLinkAltSolid /></h4>
+                  </a>
+                })}
+              </div>
+            </div>}
+          </div>
+        })}
+        </div>
+    </div>
+  )
+}
+
+export default ReactProjects
