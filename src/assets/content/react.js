9ed41a2f13bd33f4f7eb331bc8195d81e77963cc
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-02-24 17:09:48 +0000

    Created react.js

---
+++ src/assets/content/react.js
@@ -0,0 +1,125 @@
+//React
+import ImgR1 from '../media/projects/React/LuArtGalley_R.jpg';
+import ImgR2 from '../media/projects/React/WeatherApp_R.jpg';
+import ImgR3 from '../media/projects/React/TicTacToe_R.jpg';
+import ImgR4 from '../media/projects/React/DicionaryApp_R.jpg';
+import ImgR5 from '../media/projects/React/RecipeApp_R.jpg';
+import ImgR6 from '../media/projects/React/SnakeGame_R.jpg'
+import ImgR7 from '../media/projects/React/CandyHeavenShop_R.jpg';
+import ImgR8 from '../media/projects/React/BubbleCommunity_R.jpg';
+import ImgR9 from '../media/projects/React/Hangman_R.jpg';
+import ImgR10 from '../media/projects/React/Quotes_R.jpg';
+import ImgR11 from '../media/projects/React/ArtWorld_R.jpg';
+import ImgR12 from '../media/projects/React/Quiz_R.jpg';
+import ImgR13 from '../media/projects/React/MusicApp_R.jpg';
+import ImgR14 from '../media/projects/React/PawHub_R.jpg'
+import ImgR15 from '../media/projects/React/RestaurantReservations_R.jpg';
+import ImgR16 from '../media/projects/React/TodoList_R.jpg';
+import ImgR17 from '../media/projects/React/MoosAndCoos_R.jpg';
+import ImgR18 from '../media/projects/React/MovingArt_R.jpg';
+import ImgR19 from '../media/projects/React/FlowFields_R.jpg';
+
+const projects = [
+    {
+        title: 'React Projects',
+        project: [
+            {
+                title: 'LuArt Gallery',
+                href: 'https://luartgallery.com/',
+                src: ImgR1,
+            },
+            {
+                title: 'Weather App',
+                href: 'https://weather-app-react-luna-smith.netlify.app/',
+                src: ImgR2,
+            },
+            {
+                title: 'Tic Tac Toe',
+                href: 'https://tic-tac-toe-react-typescript-luna.netlify.app/',
+                src: ImgR3,
+            },
+            {
+                title: 'Dictionary App',
+                href: 'https://luminous-dictionary-app.netlify.app/',
+                src: ImgR4,
+            },
+            {
+                title: 'Recipe App',
+                href: 'https://recipe-app-react-luna-smith.netlify.app/',
+                src: ImgR5,
+            },
+            {
+                title: 'Snake Game',
+                href: 'https://snake-game-react-typescript.netlify.app/',
+                src: ImgR6,
+            },
+            {
+                title: 'Candy Heaven',
+                href: 'https://candyheavenshop.netlify.app/',
+                src: ImgR7,
+            },
+            {
+                title: 'Bubble',
+                href: 'https://community-fans.netlify.app/',
+                src: ImgR8,
+            },
+            {
+                title: 'Hangman',
+                href: 'https://hangman-react-typescript.netlify.app/',
+                src: ImgR9,
+            },
+            {
+                title: 'Quotes App',
+                href: 'https://quotes-app-luna-smith.netlify.app/',
+                src: ImgR10,
+            },
+            {
+                title: 'Art World',
+                href: 'https://example-art-gallery.netlify.app/',
+                src: ImgR11,
+            },
+            {
+                title: 'Flow Fields',
+                href: 'https://flow-fields-react.netlify.app/',
+                src: ImgR19,
+            },
+            {
+                title: 'Quiz',
+                href: 'https://react-typescript-quiz-luna-smith.netlify.app/',
+                src: ImgR12,
+            },
+            {
+                title: 'Music App',
+                href: 'https://music-app-luna-smith.netlify.app/',
+                src: ImgR13,
+            },
+            {
+                title: 'PawHub',
+                href: 'https://pawhub-react-typescript.netlify.app/',
+                src: ImgR14,
+            },
+            {
+                title: 'Reservations',
+                href: 'https://restaurant-reservation-order.netlify.app/',
+                src: ImgR15,
+            },
+            {
+                title: 'Todo List',
+                href: 'https://todo-list-react-redux-typescript.netlify.app/',
+                src: ImgR16,
+            },
+            {
+                title: 'Moos and Coos',
+                href: 'https://moosandcoosreactreduxtypescript.netlify.app/',
+                src: ImgR17,
+            },
+            {
+                title: 'Moving Art',
+                href: 'https://moving-art-exhibition.netlify.app/',
+                src: ImgR18,
+            }
+        ]
+    },
+]
+
+export default projects;
