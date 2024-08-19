38da444cc92f0dd01829777800652260ebd0fef0
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-08-19 14:49:15 +0100

    Added meta tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,25 @@
 import React from "react";
 import Home from "../components/Home/Home";
+import { Helmet } from 'react-helmet';
+import Facebook from '../assets/media/facebook.jpg';
 
 const home = ({mode, handleMode}) => {
 
   return (
     <>
+      <Helmet>
+        <title>My Page Title</title>
+        <meta name="description" content="Luna Smith portfolio - a web developer and an artist" />
+        <meta name="keywords" content="react, Luna, Smith, portfolio, web developer, artist, fine art, Scotland, games, engineer, programming" />
+        <meta property="og:title" content="Luna Smith Art portfolio" />
+        <meta property="og:description" content="una Smith portfolio - a web developer and an artist" />
+        <meta property="og:image" content={Facebook} />
+        <meta name="robots" content="index, follow" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Luna Smith Art portfolio" />
+        <meta name="twitter:description" content="Luna Smith portfolio - a web developer and an artist" />
+        <meta name="twitter:image" content={Facebook} />
+      </Helmet>
       <Home mode={mode} handleMode={handleMode} />
     </>
   );
