86e2f0ccce735d28c7f6469f1a6ee11ca14a5779
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2024-09-24 21:04:15 +0100

    Added canvas to About.tsx

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,7 @@
-import React  from 'react';
+import {useRef, useEffect}  from 'react';
 import Sidebar from '../Sidebar/Sidebar';
 import MobileSidebar from '../Sidebar/MobileSidebar';
 import Bio from './Bio';
-import Work from './Work';
 
 interface ModeProps {
   mode: string,
@@ -10,16 +9,46 @@ interface ModeProps {
 };
 
 const About = ({mode, handleMode}: ModeProps) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const context = canvas.getContext('2d');
+
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      if (context) {
+        // Example of drawing something on the canvas, like a background color or pattern
+        context.fillStyle = mode === 'light' ? 'rgb(231, 226, 233)' : 'rgb(32, 36, 41)';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+      }
+
+      const handleResize = () => {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        context?.fillRect(0, 0, canvas.width, canvas.height); 
+      };
+
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }
+  }, [mode]);
 
   return (
     <div className={`About ${mode === 'light' ? 'lightMode' : 'darkMode'}`}>
-          <div className='menu'>
+      <div className='menu'>
         <Sidebar mode={mode} handleMode={handleMode} />
       </div>
       <div className='menuMobile'>
         <MobileSidebar mode={mode} handleMode={handleMode} />
       </div>
       <div className="componentContainer">
+          <canvas ref={canvasRef} className="backgroundCanvas" />
           <Bio mode={mode} />
       </div>
     </div>
--- src/components/About/Bio.tsx
+++ src/components/About/Bio.tsx
@@ -15,13 +15,13 @@ const Bio = ({ mode }: ModeProps) => {
       };
 
   return (
-    <div>
+    <div className='Bio'>
         <h2>About</h2>
         <div className="bioContainer">
           <div className="imageContainer">
             <img src={BioImage} alt="Luna Smith" title="Luna Smith" loading="lazy" width="auto" height="auto"/>
           </div>
-          <div className="bio">
+          <div className="bioDescription">
             <p>I am a web developer and artist from Edinburgh, Scotland, combining my creative background in fine arts with modern web technologies. My journey as a creator began with a love for nature and art, winning my first competition in primary school, which set me on a lifelong path of creativity. Over the years, I’ve developed my technical skills while staying rooted in my artistic passion, specializing in building responsive, user-friendly web applications using technologies like JavaScript, Python, React, and Vue.</p> 
             <p>Living in various countries has shaped my approach to both art and coding, exposing me to diverse cultures, languages, and environments. This global perspective influences my work, whether I’m developing sleek websites or creating oil paintings. I specialize in expressive oil painting, where I use bold brushstrokes and finger techniques to capture not just the surface but the soul of my subjects. My work has been featured in exhibitions in Edinburgh, Italy, and China, and I continue to explore new creative frontiers.</p> 
             <p>As a web developer, I thrive on solving technical challenges and merging functionality with aesthetics, recently developing a fully responsive online store for LuArt Gallery. Outside of coding and art, I’m inspired by Norse mythology and Celtic culture, weaving these ancient stories and symbols into my projects. My passion for both art and technology drives me to continuously learn and push the boundaries of what’s possible in both fields.</p>
